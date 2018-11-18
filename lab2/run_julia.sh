4bf1e927235669f379464feba2ca45029110350c
oswinoswin <atori@tlen.pl>
2018-11-18 10:57:53 +0100

    run_julia.sh

--- lab2/run_julia.sh
+++ lab2/run_julia.sh
@@ -6,8 +6,15 @@
 #SBATCH -J juliaRun
 module add plgrid/tools/julia/1.0.0
 cd $SLURM_SUBMIT_DIR
+repeats = 10
 for P in {1..12}
 do
-    julia -p $P  distributed.jl >> distributed_time.txt
-    julia -p $P  pmap.jl >> pmap_time.txt
+    for rep in {1..repeats}
+    do
+        time_distributed=$(julia -p $P  distributed.jl)
+        time_pmap=$(julia -p $P  pmap.jl)
+        echo "$P $time_distributed" >> distributed_results.txt
+        echo "$P $time_pmap" >> pmap_results.txt
+    done
+
 done
